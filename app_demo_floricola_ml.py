17d8810ee5633c2e729231567e9889ab5b8e954c
vikpalm07-boop <vikpalm07@gmail.com>
2026-04-29 16:07:22 -0500

    app_demo_floricola_ml.py

--- app_demo_floricola_ml.py
+++ app_demo_floricola_ml.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
-
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
